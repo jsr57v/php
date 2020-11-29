d8dc483add7adb4bc0eb40d6fe572600c728e07c
jsr57v <70434822+jsr57v@users.noreply.github.com>
2020-11-29 21:40:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-$target = getenv('TARGET', true) ?: 'World';
-echo sprintf("Hello %s!", $target);
+ <?php
+
+$array = [];
+
+for ($i = 0; $i < rand(1000, 10000); $i++) {
+    $array[$i] = rand(1000, 10000);
+}
+
+shuffle($array);
